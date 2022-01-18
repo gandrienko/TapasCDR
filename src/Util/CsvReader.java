ba30c4f28f6259095c6177a626cf17a4aaedaf18
gandrienko <gennady.andrienko@iais.fraunhofer.de>
2022-01-18 17:06:22 +0100

    loading data - in progress

--- src/Util/CsvReader.java
+++ src/Util/CsvReader.java
@@ -1,9 +1,11 @@
 package Util;
 
 import java.io.*;
+import java.util.Vector;
 
 public class CsvReader {
   String columns[]=null;
+  Vector<String[]> rows=null;
   public CsvReader (String path, String fname) {
     try {
       read(path+"\\"+fname);
@@ -15,6 +17,11 @@ public class CsvReader {
     BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(new File(fname))));
     String strLine=br.readLine();
     columns=strLine.split(",");
+    rows=new Vector<>(100,100);
+    while ((strLine = br.readLine()) != null) {
+      String values[]=strLine.split(",");
+      rows.add(values);
+    }
     br.close();
   }
   public int getNColumns() { return (columns==null) ? 0 : columns.length; }
@@ -25,5 +32,14 @@ public class CsvReader {
       if (column.equals(columns[i]))
         return i;
     return -1;
+  }
+  public String getValue (int row, int columnN) {
+    return rows.elementAt(row)[columnN].trim();
+  }
+  public double getValueAsDouble (int row, int columnN) {
+    return Double.valueOf(getValue(row,columnN)).doubleValue();
+  }
+  public double getValueAsInt (int row, int columnN) {
+    return Integer.valueOf(getValue(row,columnN)).intValue();
   }
 }
