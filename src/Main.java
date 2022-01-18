1d71da4f1ffdc9d48fcd63d6a870ad5a14387ba4
gandrienko <gennady.andrienko@iais.fraunhofer.de>
2022-01-18 18:10:20 +0100

    loading data - in progress

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,8 @@ public class Main {
     if (args!=null && args.length==1) {
       String path=args[0];
       System.out.println(path);
-      CsvReader csvMain=new CsvReader(path,"main.csv");
+      CsvReader csvMain=new CsvReader(path,"main.csv"),
+                csvConflicts=new CsvReader(path,"conflicts.csv");
       //System.out.println("Main.csv: "+csvMain.getNColumns()+" columns, "+csvMain.getNRows()+" rows");
     }
   }
